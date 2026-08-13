b40b3ae5aa19428c0a9379e62def651f1177fc8e
7UcA5TE2 <inamm177@gmail.com>
2026-08-13 23:58:35 +0530

    started working on project home page and add some files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,19 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import HomePage from './landing_page/home/HomePage.js';
-import AboutPage from './landing_page/about/AboutPage.js';
-import PricingPage from './landing_page/pricing/PricingPage.js';
-import ProductPage from './landing_page/product/ProductPage.js';
-import SupportPage from './landing_page/support/SupportPage.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <HomePage/>
-      <AboutPage/>
-      <PricingPage/>
-      <ProductPage/>
-      <SupportPage/>
   </React.StrictMode>
 );
 
--- frontend/src/landing_page/home/awards.js
+++ frontend/src/landing_page/home/awards.js
@@ -1,8 +1,43 @@
 import React from 'react';
 function Awards() {
-  return ( 
-    <h1>awards</h1>
-   );
+  return (
+    <div className='container'>
+      <div className='row'>
+
+        <div className='col p-5'>
+          <img src="./media/images/largestBroker.svg" className="img-fluid mb-5" alt="Hero_image" />
+        </div>
+
+        <div className='col p-5'>
+
+          <div className='row mb-5 mt-3'>
+            <h2>Largest stock broker in India</h2>
+            <p>2+ million Trade-X Clients contribute to over 15% of all retail order volumes in india daily by trading and investing in: </p>
+          </div>
+
+          <div className='row'>
+
+            <div className='col'>
+              <ul className="list">
+                <li className="list-item mb-2">Futures and options</li>
+                <li className="list-item mb-2">Commodity derivatives</li>
+                <li className="list-item mb-2">Currency derivatives</li>
+              </ul>
+            </div>
+
+            <div className='col mb-5'>
+              <li className="list-item mb-2">Stocks and IPOs</li>
+              <li className="list-item mb-2">Direct mutual funds</li>
+              <li className="list-item mb-2">Bonds and Govt. Securities</li>
+            </div>
+
+            <img src="./media/images/Presslogos.png" className="img-fluid mb-5" alt="Hero_image" style={{ width: "90%" }} />
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Awards;
