1814824d7d364a8dcc0b04cb4424e676745be6f7
7UcA5TE2 <inamm177@gmail.com>
2026-08-15 11:19:23 +0530

    added pricing section in home

--- frontend/src/landing_page/home/pricing.js
+++ frontend/src/landing_page/home/pricing.js
@@ -1,6 +1,30 @@
 import React from 'react';
 function Pricing() {
-  return ( <h1>pricing</h1> );
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-5 ps-5'>
+          <h1 className='mb-5 fs-2'>Unbeatable pricing</h1>
+          <p>We pioneered the concept of discount broking and price
+            transparency in India. Flat fees and no hidden charges.</p>
+          <a href='' style={{ textDecoration: 'none' }}>See pricing <i className="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+        </div>
+        <div className='col-1'></div>
+        <div className='col-6 px-5 py-3'>
+          <div className='row text-center'>
+            <div className='col border'>
+              <h1 className='mb-5 mt-4 fs-2'>$0</h1>
+              <p>Free equity delivery and direct funds</p>
+            </div>
+            <div className='col border'>
+              <h1 className='mb-5 mt-4 fs-2'>$20</h1>
+              <p>Intraday and F&O</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Pricing;
