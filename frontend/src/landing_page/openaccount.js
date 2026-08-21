bd26f18b5ba5ef2cae99938a8c74383a9146167f
7UcA5TE2 <inamm177@gmail.com>
2026-08-21 23:45:17 +0530

    added openaccount component

--- frontend/src/landing_page/openaccount.js
+++ frontend/src/landing_page/openaccount.js
@@ -1,6 +1,15 @@
 import React from 'react';
 function OpenAccount() {
-  return ( <h1>openaccount</h1> );
+  return (
+    <div className="container p-5 text-center mb-5">
+      <div className='row'>
+        <h1 className='mt-5'>Open a Trade-X account</h1>
+        <p>Modern trading platform and powerful apps. Start investing with
+          zero account opening fees and simple, flat pricing on trades.</p>
+        <button type="button" className="btn btn-dark w-25  mx-auto p-2 mt-3 fs-5">Sign up now</button>
+      </div>
+    </div>
+  );
 }
 
 export default OpenAccount;
