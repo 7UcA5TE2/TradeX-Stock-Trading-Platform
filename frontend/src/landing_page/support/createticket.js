be8360d653febb4672492294244a654f7120c6b5
7UcA5TE2 <inamm177@gmail.com>
2026-09-05 23:16:40 +0530

    added frame for ticket section in support page

--- frontend/src/landing_page/support/createticket.js
+++ frontend/src/landing_page/support/createticket.js
@@ -1,6 +1,41 @@
 import React from 'react';
 function CreateTicket() {
-  return ( <h1>createticket</h1> );
+  return (
+    <div className='container mt-3 p-5'>
+      <div className='row'>
+        <h4>To create a ticket, select a relevant topic</h4>
+      </div>
+      <div className='row'>
+        <div className='col'>
+
+          <a href='' style={{ textDecoration: "none", color: "black" }}>Account Opening</a>
+          <ul className='list'>
+            <li>Online Account Opening</li>
+
+            <li>Offline Account Opening</li>
+
+            <li>Company, Partnership and HUF Account
+              Opening</li>
+
+            <li>NRI Account Opening</li>
+
+            <li>Charges at Zerodha</li>
+
+            <li>Zerodha IDFC FIRST Bank 3-in-1 Account</li>
+          </ul>
+        </div>
+        <div className='col'>
+          <a href='' style={{ textDecoration: "none", color: "black" }}>Your Trade-X Account</a>
+
+        </div>
+        <div className='col'>
+          <a href='' style={{ textDecoration: "none", color: "black" }}>Your Trade-X Account</a>
+
+        </div>
+      </div>
+
+    </div>
+  );
 }
 
 export default CreateTicket;
